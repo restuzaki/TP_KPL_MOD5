1f418f56d4c9e5cf2dd23086eb8bee6d0e494dab
Damai <damaiputrayudha1@gmail.com>
2025-03-16 00:32:01 +0700

    menambahkan class DataGeneric

--- tpmodul5_103022300029/Program.cs
+++ tpmodul5_103022300029/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class main
+{
+    static void Main(string[] args)
+    {
+        HaloGeneric nama = new HaloGeneric();
+        nama.sapaUser<string>("Damai");
+        DataGeneric<string> nim = new DataGeneric<string>("103022300029");
+        nim.printData();
+    }
+}
+
+class DataGeneric<T>
+{
+    T data;
+
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+
+    public void printData()
+    {
+        Console.WriteLine($"Data yang tersimpan adalah: {data}");
+    }
+}
+
+class HaloGeneric()
+{
+    public void sapaUser<T>(T user)
+    {
+        Console.WriteLine($"Halo User {user}");
+    }
+}
